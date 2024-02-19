8fb88da10ecb84d336285911861a371129dd8e0c
MattiaVolonterio <volonteriomattia@gmail.com>
2024-02-19 15:34:23 +0100

    feat: jumbotron

--- src/components/AppJumbo.vue
+++ src/components/AppJumbo.vue
@@ -8,7 +8,55 @@ export default {
 </script>
 
 <template>
-  <JumboBackground></JumboBackground>
+  <div class="jumbo-container">
+    <JumboBackground />
+    <div class="jumbo-content">
+      <div class="title marck-script-regular">Hello, Im Matin</div>
+      <div class="subtitle">
+        Artist Coaching And Mentoring Might Be for You.
+      </div>
+      <div class="btn-orange">Get started today</div>
+    </div>
+  </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@use "../styles/partials/mixins" as *;
+@use "../styles/partials/variables" as *;
+
+.jumbo-container {
+  position: relative;
+
+  .jumbo-content {
+    @include flex-column-center;
+
+    position: absolute;
+    z-index: 1;
+    width: 35%;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+
+    text-align: center;
+
+    .title {
+      color: $sectionTitle;
+      font-family: "Marck Script", cursive;
+      font-size: 2.5rem;
+    }
+
+    .subtitle {
+      font-weight: bold;
+      font-size: 2rem;
+      color: $lightBlack;
+
+      margin-bottom: 20px;
+    }
+
+    .btn-orange {
+      padding-top: 8px;
+      padding-bottom: 8px;
+    }
+  }
+}
+</style>
