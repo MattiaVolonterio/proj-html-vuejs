cb6ab25f40c636c8c87a54d99233faee22360b34
MattiaVolonterio <volonteriomattia@gmail.com>
2024-02-19 18:06:58 +0100

    fix: citation width

--- src/components/artistCoaching/AppCoachingCit.vue
+++ src/components/artistCoaching/AppCoachingCit.vue
@@ -13,29 +13,26 @@ export default {
 </script>
 <template>
   <div class="container">
-    <div class="row">
-      <div class="col-6 col-left">
-        <img :src="getImgPath('artist-quote-icon.png')" alt="quotes" />
-        <p>
-          I coach and mentor visual artists, like you, to Create Like You Mean
-          It
-        </p>
-        <div class="title">Martin Garrix</div>
-      </div>
-      <div class="col-6 col-right">
-        <p>
-          As a coach and mentor, my ultimate goal is to motivate visual artists
-          toward inspired action. I help you get you back on track to being the
-          productive, creative, meaningful and authentic artist you truly want
-          to be. I support artists in crafting and living a creative life as
-          well as feeling good about themselves and thei art.
-        </p>
-        <p>
-          As an artist and educator for nearly 30 years, I understand what it
-          takes to create.
-        </p>
-        <div class="btn-orange-light">Get started today</div>
-      </div>
+    <div class="col-left">
+      <img :src="getImgPath('artist-quote-icon.png')" alt="quotes" />
+      <p>
+        I coach and mentor visual artists, like you, to Create Like You Mean It
+      </p>
+      <div class="title">Martin Garrix</div>
+    </div>
+    <div class="col-right">
+      <p>
+        As a coach and mentor, my ultimate goal is to motivate visual artists
+        toward inspired action. I help you get you back on track to being the
+        productive, creative, meaningful and authentic artist you truly want to
+        be. I support artists in crafting and living a creative life as well as
+        feeling good about themselves and thei art.
+      </p>
+      <p>
+        As an artist and educator for nearly 30 years, I understand what it
+        takes to create.
+      </p>
+      <div class="btn-orange-light">Get started today</div>
     </div>
   </div>
 </template>
@@ -45,8 +42,13 @@ export default {
 @use "../../styles/partials/variables" as *;
 
 .container {
+  @include space-between;
+  gap: 80px;
   margin-bottom: 7rem;
-  .col-6.col-left {
+  .col-left {
+    flex-grow: 0;
+    flex-shrink: 0;
+    width: 40%;
     text-align: center;
     p {
       font-size: 2.5rem;
@@ -59,8 +61,10 @@ export default {
     }
   }
 
-  .col-6.col-right {
+  .col-right {
     @include flex-column-start;
+    flex: 1 0;
+    width: 60%;
 
     p {
       color: $headerList;
